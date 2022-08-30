485416fdb44b232edd185a460a7ba91b75fe1ab0
Sakthivel M <msv300@gmail.com>
2022-08-30 12:46:50 +0200

    Factorial with iterative approach

--- src/algos/recursion/fibonacci/index.js
+++ src/algos/recursion/fibonacci/index.js
@@ -2,6 +2,8 @@
 // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
 // Ref: https://www.mathsisfun.com/numbers/fibonacci-sequence.html
 // 
+// Using Recursion
+// 
 function fibonacci(n, computed={ 0: 0, 1: 1 }) {
   if (n <= 1) return n;
   else {
@@ -9,5 +11,17 @@ function fibonacci(n, computed={ 0: 0, 1: 1 }) {
     return computed[n];
   }
 };
+
+// Using Iteration
+// 
+// function fibonacci(n) {
+//   const arr = [0, 1];
+
+//   for(i = 2; i <= n; i++) {
+//     arr[i] = arr[i - 1] + arr[i - 2];
+//   }
+
+//   return arr[n];
+// }
 
 module.exports = fibonacci;
