84d23e10d40a2af8478a426b27985f3c98f02d07
Sakthivel M <msv300@gmail.com>
2022-09-05 22:01:23 +0200

    Selection Sort

---
+++ src/algos/sorting/selection/index.js
@@ -0,0 +1,15 @@
+module.exports = function sort(arr) {
+  for(let i = 0; i < arr.length; i++) {
+    let min = i;
+
+    for(let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) min = j;
+    }
+
+    const temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+
+  return arr;
+}
