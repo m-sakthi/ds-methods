1d13f62db2bde9f7fc132f0a2ee2e00d0d1af723
Sakthivel M <msv300@gmail.com>
2022-09-13 11:37:18 +0200

    Merge Sort

---
+++ src/algos/sorting/merge/index.js
@@ -0,0 +1,35 @@
+// Merge sort uses divide and conquere method where
+// we divide the list into two until it is down to single element
+// and smaller list is sorted and merged back.
+// 
+// Time Complexity - θ(n log(n))
+// Space Complexity - O(n)
+//
+function merge(left, right) {
+  const result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while(leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++
+    }
+  }
+
+  return result.concat(left.slice(leftIndex), right.slice(rightIndex));
+}
+
+module.exports = function sort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const length = arr.length;
+  const middle = Math.floor(length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(sort(left), sort(right));
+}
---
+++ src/algos/sorting/merge/index.test.js
@@ -0,0 +1,27 @@
+const sort = require('./index');
+
+describe("sort", () => {
+  it("should return empty array if passed array string", () => {
+    expect(sort([])).toEqual([]);
+  });
+
+  it("should sort the given array in ascending order", () => {
+    expect(sort([3, 2, 1])).toEqual([1, 2, 3]);
+  });
+
+  it("should sort the given array with 10 elements", () => {
+    expect(sort([1, 10, 4, 2, 9, 5, 6, 8, 3, 0, 7])).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it("should sort the given array with 50 elements", () => {
+    const sortedArray = Array(50).fill().map((_, i) => i);
+    const unsortedArray = [...sortedArray].sort(() => 0.5 - Math.random());
+    expect(sort(unsortedArray)).toEqual(sortedArray);
+  });
+
+  it("should sort the given array with 100 elements", () => {
+    const sortedArray = Array(100).fill().map((_, i) => i);
+    const unsortedArray = [...sortedArray].sort(() => 0.5 - Math.random());
+    expect(sort(unsortedArray)).toEqual(sortedArray);
+  });
+});
